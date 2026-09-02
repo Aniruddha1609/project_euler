f81d6f4e48d40b437d6a4b6af957c67171f35bc9
Aniruddha1609 <anniruddha2003@gmail.com>
2026-09-02 18:08:33 +0530

    Update largest_prime_factor.js to find the largest prime factor of 600851475143

--- largest_prime_factor.js
+++ largest_prime_factor.js
@@ -1,21 +1,28 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143?
 
-const num = 13195;
-
-const isPrime = (num) => {
-    for (let curr = 2; curr < (Math.sqrt(num)); curr++) {
-        if (num % curr === 0) return false;
-    }
-    return true;
-};
+const num = 600851475143;
 
 const largestPrimeFactors = (num) => {
-    let max = 1;
-    for (let curr = 2; curr < Math.floor(num / 2); curr++) {
-        if (num % curr === 0 && curr > max && isPrime(curr)) max = curr;
+    let largestPrime = -1;
+
+    while (num % 2 == 0) {
+        largestPrime = 2;
+        num /= 2;
     }
-    return max;
+
+    for (let curr = 3; curr * curr <= num; curr += 2) {
+        while (num % curr === 0) {
+            largestPrime = curr;
+            num /= curr;
+        }
+    }
+
+    if (num > 2) {
+        largestPrime = num;
+    }
+
+    return largestPrime;
 };
 
 const max = largestPrimeFactors(num);
