8ffc5aa304429200b80dd1bd7915d7d3d7cc57a3
Aniruddha1609 <anniruddha2003@gmail.com>
2026-09-18 16:33:41 +0530

    Add initial implementation of lattice paths calculation

---
+++ lattice_paths.js
@@ -0,0 +1,32 @@
+// Starting in the top left corner of a 2 * 2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+// How many such routes are there through a 20 * 20 grid?
+
+let ROWLIMIT;
+let COLOUMNLIMIT;
+
+const route = (row, column, count) => {
+    if (row === ROWLIMIT && column === COLOUMNLIMIT) {
+        console.log(count);
+
+        count.push(count[0] + 1);
+        count.shift();
+        return;
+    }
+    if (row !== ROWLIMIT && column !== COLOUMNLIMIT) {
+        route(row, column + 1, count);
+        route(row + 1, column, count);
+    }
+    if (row === ROWLIMIT) route(row, column + 1, count);
+    if (column === COLOUMNLIMIT) route(row + 1, column, count);
+};
+
+const routeCount = (rowLimit, columnLimit) => {
+    ROWLIMIT = rowLimit;
+    COLOUMNLIMIT = columnLimit;
+    let count = [0];
+    route(0, 0, count);
+    return count;
+};
+
+const [count] = routeCount(5, 5);
+console.log(count);
