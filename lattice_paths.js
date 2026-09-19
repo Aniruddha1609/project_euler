ca24d3ebbb41e41512b98b573e5eedbec6aa9a24
Aniruddha1609 <anniruddha2003@gmail.com>
2026-09-19 18:25:46 +0530

    Fix row and column limits in routeCount function for accurate lattice path calculation

--- lattice_paths.js
+++ lattice_paths.js
@@ -19,12 +19,12 @@ const route = (row, column, count) => {
 };
 
 const routeCount = (rowLimit, columnLimit) => {
-    ROWLIMIT = rowLimit - 1;
-    COLOUMNLIMIT = columnLimit - 1;
+    ROWLIMIT = rowLimit;
+    COLOUMNLIMIT = columnLimit;
     let count = [0];
     route(0, 0, count);
     return count;
 };
 
-const [count] = routeCount(3, 3);
+const [count] = routeCount(2, 2);
 console.log(count);
